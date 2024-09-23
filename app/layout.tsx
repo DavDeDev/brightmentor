18427f3cbdf1cf1d8b6af4d8e52322cbb987942f
David Pietrocola <pietrocoladavid@gmail.com>
2024-09-23 13:18:49 -0400

    feat: completes landing page

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,8 @@ import { GeistSans } from "geist/font/sans";
 import { ThemeProvider } from "next-themes";
 import Link from "next/link";
 import "./globals.css";
+import Hero from "@/components/hero";
+import { SiteFooter } from "@/components/site-footer";
 
 const defaultUrl = process.env.VERCEL_URL
   ? `https://${process.env.VERCEL_URL}`
@@ -31,28 +33,8 @@ export default function RootLayout({
           disableTransitionOnChange
         >
           <SiteHeader/>
-          <main className="min-h-screen flex flex-col items-center">
-            <div className="flex-1 w-full flex flex-col gap-20 items-center">
-              <div className="flex flex-col gap-20 max-w-5xl p-5">
-                {children}
-              </div>
-
-              <footer className="w-full flex items-center justify-center border-t mx-auto text-center text-xs gap-8 py-16">
-                <p>
-                  Powered by{" "}
-                  <a
-                    href="https://supabase.com/?utm_source=create-next-app&utm_medium=template&utm_term=nextjs"
-                    target="_blank"
-                    className="font-bold hover:underline"
-                    rel="noreferrer"
-                  >
-                    Supabase
-                  </a>
-                </p>
-                <ThemeSwitcher />
-              </footer>
-            </div>
-          </main>
+          <Hero/>
+          <SiteFooter/> 
         </ThemeProvider>
       </body>
     </html>
